a8a247536c95a2c4ded656555d7eca3d0687020d
canbesad <minehammer26@gmail.com>
2025-03-13 10:49:58 +0900

    1234

---
+++ 원피스 이즈 리얼/test.py
@@ -0,0 +1,5 @@
+import sys
+
+print("이것은 vi 에디터이다 이말입니다.")
+
+
---
+++ 흑화당한 파일들.../내왼손에는흑염룡이잠들어있다.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int n;
+typedef struct Tree {
+
+} Tree;
+
+int main(void) {
+    
+
+    return 0;
+}
+
+// 일단 그래프랑 dp 엮은 것부터가 수상하다.
+// 
