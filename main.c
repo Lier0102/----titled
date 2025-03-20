90e367f1d6661f27219f2098319006bcbceb3b16
canbesad <minehammer26@gmail.com>
2025-03-20 09:23:47 +0900

    sasdf

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d.", a);
+    printf("%02d.", b);
+    printf("%02d", c);
 
     return 0;
+
 }
