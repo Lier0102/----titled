8d75d17482835d5524517eb21ece29f84a89c84e
canbesad <minehammer26@gmail.com>
2025-03-24 18:04:27 +0900

    ㅇㅇㅇ

--- testses.c
+++ testses.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-    int total;
-    char grade;
+    int a, b, c;
+    int n1000=0, n500=0, n100=0;
 
-    scanf("%d", &total);
-    scanf(" %c", &grade);
+    scanf("%d %d", &a, &b);
 
-    printf("%d %c", total, grade);
+    c = b - a;
 
-    return 0;
-    
+    while(1) {
+        if (c - 1000 >= 0) { c -= 1000;  n1000++;}
+        else if (c - 500 >= 0) { c -= 500; n500++;}
+        else if (c - 100 >= 0) { c -= 100; n100++;}
+        else break;
+    }
+
+    printf("1000원 : %d, 500원 : %d, 100원 : %d", n1000, n500, n100);
+}
